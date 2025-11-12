c4e0db4efcad608925eacb6bc5d55eddc83f9be6
Pratham Shrivastav <prathamshrivastav52@gmail.com>
2025-11-12 17:16:10 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
-import createMDX from '@next/mdx';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 };
 
-const withMDX = createMDX({
-  // Add MDX options...
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
